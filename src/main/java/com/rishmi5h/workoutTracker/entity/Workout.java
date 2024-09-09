70ec91929ac61bb8608619b2a6ace71dc26256e9
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-10 04:46:13 +0530

    Update Workout.java

--- src/main/java/com/rishmi5h/workoutTracker/entity/Workout.java
+++ src/main/java/com/rishmi5h/workoutTracker/entity/Workout.java
@@ -4,10 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
 
 @Data
 @Builder
@@ -25,4 +32,15 @@ public class Workout {
     private String workoutType;
     private String workoutCategory;
     private String workoutDuration;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "workout_id")
+    private List<Exercise> exercises = new ArrayList<>();
+    
+    private LocalDateTime scheduledDateTime;
+    private boolean isCompleted;
 }
