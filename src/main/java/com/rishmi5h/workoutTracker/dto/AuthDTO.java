3f75e8d5bc3729f710c4e0931e5625386a980fb3
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-10 04:34:54 +0530

    Create AuthDTO.java

---
+++ src/main/java/com/rishmi5h/workoutTracker/dto/AuthDTO.java
@@ -0,0 +1,11 @@
+package com.rishmi5h.workoutTracker.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthDTO {
+    public String username;
+    public String password;
+}
