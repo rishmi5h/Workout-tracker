6dcd594b8bdc9402d367e25ed80d6cc89aaf0c5a
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-10 04:46:17 +0530

    Update WorkoutRepository.java

--- src/main/java/com/rishmi5h/workoutTracker/repository/WorkoutRepository.java
+++ src/main/java/com/rishmi5h/workoutTracker/repository/WorkoutRepository.java
@@ -1,9 +1,14 @@
 package com.rishmi5h.workoutTracker.repository;
 
+import com.rishmi5h.workoutTracker.entity.User;
 import com.rishmi5h.workoutTracker.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+    List<Workout> findByUserOrderByScheduledDateTimeAsc(User user);
+    List<Workout> findByUserAndIsCompletedTrue(User user);
 }
