4b7247287611caf01897fdd2a54513bbf4eb91bf
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-09 05:36:20 +0530

    initial commit

---
+++ src/main/java/com/rishmi5h/workoutTracker/WorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rishmi5h.workoutTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishmi5h/workoutTracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishmi5h.workoutTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
