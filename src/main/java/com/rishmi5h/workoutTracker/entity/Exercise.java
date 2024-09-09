784a1da49d99ebdd35eda460297c7c291d59e904
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-10 04:46:06 +0530

    Update Exercise.java

--- src/main/java/com/rishmi5h/workoutTracker/entity/Exercise.java
+++ src/main/java/com/rishmi5h/workoutTracker/entity/Exercise.java
@@ -1,7 +1,6 @@
 package com.rishmi5h.workoutTracker.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,4 +14,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "t_exercise")
 public class Exercise {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int sets;
+    private int reps;
+    private double weight;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id")
+    private Workout workout;
 }
