31c9cc208f3b2c9e9717c884ab4d49ab9115ee0e
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-10 04:35:01 +0530

    Update UserRepository.java

--- src/main/java/com/rishmi5h/workoutTracker/repository/UserRepository.java
+++ src/main/java/com/rishmi5h/workoutTracker/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.rishmi5h.workoutTracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByUsername(String username);
 }
