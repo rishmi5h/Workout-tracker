afd4041767f9aed29607d3cff6f5f146032add75
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-10 05:50:09 +0530

    Update AuthDTO.java

--- src/main/java/com/rishmi5h/workoutTracker/dto/AuthDTO.java
+++ src/main/java/com/rishmi5h/workoutTracker/dto/AuthDTO.java
@@ -1,11 +1,16 @@
 package com.rishmi5h.workoutTracker.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class AuthDTO {
     public String username;
     public String password;
+
 }
